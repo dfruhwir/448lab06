f5bb71e3d0e36402e9514a7b31e863feb4670c65
Dillon Fruhwirth <d487f080@ku.edu>
2016-03-28 19:17:34 -0500

    Write file and check for user file

--- Vote/src/Vote.java
+++ Vote/src/Vote.java
@@ -2,7 +2,9 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.Component;
 import java.awt.GridLayout;
-import java.util.Random;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class Vote 
 {
@@ -13,6 +15,7 @@ public class Vote
 	private JLabel label1;
 	private JLabel label2;
 	private JLabel label3;
+	private JLabel label4;
 	private JRadioButton clinton;
 	private JRadioButton trump;
 	private JRadioButton sanders;
@@ -22,12 +25,13 @@ public class Vote
 	public Vote()
 	{
 		panel = new JPanel();
-		button = new JButton("Submit");
+		button = new JButton("Vote!");
 		fname = new JTextField(10);
 		lname = new JTextField(10);
 		label1 = new JLabel("First name:");
 		label2 = new JLabel("Last name:");
 		label3 = new JLabel("Please choose a candidate:");
+		label4 = new JLabel();
 		clinton = new JRadioButton("Hillary Clinton");
 		trump = new JRadioButton("Donald Trump");
 		sanders = new JRadioButton("Bernie Sanders");
@@ -54,6 +58,7 @@ public class Vote
 		panel.add(sanders);
 		panel.add(cruz);
 		panel.add(button);
+		panel.add(label4);
 	}
 	
 	public Component getContent()
@@ -67,7 +72,50 @@ public class Vote
 		{
 			public void actionPerformed(ActionEvent e)
 			{
-				
+				String f = fname.getText();
+				String l = lname.getText();
+				if(f.equals(""))
+				{
+					String newText = "Please enter a first name";
+            		label4.setText(newText);
+            		System.out.println(newText);
+				}
+				else if(l.equals(""))
+				{
+					String newText = "Please enter a last name";
+            		label4.setText(newText);
+            		System.out.println(newText);
+				}
+				else
+				{
+					try
+					{
+						FileReader myFileReader = new FileReader(l + "_" + f + "_ballot.txt");
+						String newText = "User already voted";
+	            		label4.setText(newText);
+	            		System.out.println(newText);
+					}
+					catch (Exception ex)
+					{
+						try
+						{
+							FileWriter fstream = new FileWriter(l + "_" + f + "_ballot.txt");
+							BufferedWriter out = new BufferedWriter(fstream);
+							out.write("Hillary Clinton");
+							out.close();
+							String newText = "Vote casted!";
+		            		label4.setText(newText);
+		            		System.out.println(newText);
+						}
+						catch(Exception ex2)
+						{
+							String newText = "Error: " + ex2.getMessage();
+		            		label4.setText(newText);
+		            		System.out.println(newText);
+						}
+					}
+
+				}
 			}
 		};
 		
